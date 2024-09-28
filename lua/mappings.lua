4784ced8a76ed474453987b506c265ab60aae229
swsxm <129607562+swsxm@users.noreply.github.com>
2024-09-29 01:50:47 +0200

    -

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,7 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("t", "kj", "<C-\\><C-N>", { desc = "exit terminal mode with kj" })
+map("t", "jk", "<C-\\><C-N>", { desc = "exit terminal mode with kj" })
 map("n", "<S-L>", "<C-w>l", { desc = "Move to right split" })
 map("n", "<S-R>", "<C-w>h", { desc = "Move to left split" })
 map("n", "<S-U>", "<C-w>k", { desc = "Move to upper split" })
