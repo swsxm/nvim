efe90599d1d8f740d708d2695d014150ad872fac
swsxm <129607562+swsxm@users.noreply.github.com>
2024-09-29 01:03:01 +0200

    Terminal exit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("t", "kj", "<C-\\><C-N>", { desc = "exit terminal mode with kj" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
