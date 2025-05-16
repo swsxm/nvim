7a644c3944d566756070e8dd801e14fc1b89f1ae
swsxm <129607562+swsxm@users.noreply.github.com>
2025-05-16 22:43:04 +0200

    colortheme change

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,59 +1,10 @@
 return {
-	"RRethy/base16-nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		-- Define your custom Base16 color palette
-		require("base16-colorscheme").setup({
-			base00 = "#000000",
-			base01 = "#121212",
-			base02 = "#222222",
-			base03 = "#333333",
-			base04 = "#999999",
-			base05 = "#ffffff",
-			base06 = "#999999",
-			base07 = "#c1c1c1",
-			base08 = "#ffffff",
-			base09 = "#aaaaaa",
-			base0A = "#e78a53",
-			base0B = "#fbcb97",
-			base0C = "#aaaaaa",
-			base0D = "#888888",
-			base0E = "#999999",
-			base0F = "#444444",
-		})
-		require("base16-colorscheme").with_config({
-			telescope = true,
-			indentblankline = true,
-			notify = true,
-			ts_rainbow = true,
-			cmp = true,
-			illuminate = true,
-			dapui = true,
-		})
-
-		vim.g.colors_name = "base16-black-metal-custom"
-	end,
+	{
+		"sjl/badwolf",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme badwolf")
+		end,
+	},
 }
-
--- return {
--- 	"RRethy/base16-nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		-- Basic setup using a built-in theme (replace later)
---
--- 		-- Configuration for plugin integrations (enable as needed)
--- 		require("base16-colorscheme").with_config({
--- 			telescope = true,
--- 			indentblankline = true,
--- 			notify = true,
--- 			ts_rainbow = true,
--- 			cmp = true,
--- 			illuminate = true,
--- 			dapui = true,
--- 		})
---
--- 		vim.cmd("colorscheme base16-black-metal-immortal")
--- 	end,
--- }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,8 +5,17 @@ return {
 			view_options = {
 				show_hidden = true,
 			},
+			columns = {
+				"icon",
+				"permissions",
+				"size",
+				"mtime",
+			},
+			format_mtime = "%Y-%m-%d %H:%M",
 		},
-		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		dependencies = {
+			{ "echasnovski/mini.icons", opts = {} },
+		},
 	},
 	vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>"),
 }
