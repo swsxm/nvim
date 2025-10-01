f339d89eb5504bd641c90970310b94e85543dfe2
swsxm <129607562+swsxm@users.noreply.github.com>
2025-10-01 23:37:57 +0200

    add: new colortheme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,45 +1,11 @@
+-- lua/plugins/gruvbox_vim.lua
 return {
-	{
-		"sjl/badwolf",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.opt.background = "dark"
-			vim.cmd("colorscheme badwolf")
-
-			-- Override background for core UI areas
-			local black = "#1c1f20"
-			vim.api.nvim_set_hl(0, "Normal", { bg = black })
-			vim.api.nvim_set_hl(0, "NormalNC", { bg = black })
-			vim.api.nvim_set_hl(0, "SignColumn", { bg = black })
-			vim.api.nvim_set_hl(0, "LineNr", { bg = black })
-			vim.api.nvim_set_hl(0, "VertSplit", { bg = black })
-			vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = black })
-		end,
-	},
+	"morhetz/gruvbox",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.gruvbox_contrast_dark = "hard" -- options: "hard", "medium", default ""
+		vim.o.background = "dark"
+		vim.cmd("colorscheme gruvbox")
+	end,
 }
-
--- solarized8
---
--- return {
--- 	"lifepillar/vim-solarized8",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		-- This theme is Vimscript-based and uses global variables for configuration.
--- 		-- Set them BEFORE setting the colorscheme.
---
--- 		-- Optional: Use the 256-color palette for improved terminal compatibility.
--- 		-- The default is 16. Most modern terminals support 256.
--- 		vim.g.solarized_termcolors = 256
---
--- 		-- Enable true color support in Neovim
--- 		vim.o.termguicolors = true
---
--- 		-- Set the background to 'light'. This is what triggers the light variant.
--- 		vim.o.background = "light"
---
--- 		-- Finally, apply the colorscheme
--- 		vim.cmd.colorscheme("solarized8")
--- 	end,
--- }
