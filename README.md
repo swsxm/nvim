e544d94a93f8b8e1ef77e4095315c24de3bfb840
swsxm <129607562+swsxm@users.noreply.github.com>
2025-12-21 23:39:20 +0100

    vscode setup

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
-# Make sure nvim prerequisites are installed
-
-# Install Nerd Font
-
-# Install npm
-
-# Install Node
-
-# Install zip
-
-# Install sudo apt install -y python3-venv
+-- TODO --
+Docs: keybinds to add
--- init.lua
+++ init.lua
@@ -1,30 +1,43 @@
-require("core.options")
-require("core.keymaps")
+vim.g.mapleader = " "
+vim.opt.clipboard = "unnamedplus"
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+vim.keymap.set("n", "x", '"_x', opts)
+vim.keymap.set("v", "p", '"_dP', opts)
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+
+if vim.g.vscode then
+    local vscode = require('vscode')
+
+    vim.keymap.set('n', '<leader>sf', function() vscode.action('workbench.action.quickOpen') end)
+    vim.keymap.set('n', '<leader>sg', function() vscode.action('workbench.action.findInFiles') end)
+    vim.keymap.set('n', '<leader>sd', function() vscode.action('actions.find') end)
+    vim.keymap.set('n', '<leader>sb', function() vscode.action('workbench.action.showAllEditorsByAppearance') end)
+    vim.keymap.set('n', '<leader>ss', function() vscode.action('workbench.action.gotoSymbol') end)
+    vim.keymap.set('n', '<leader>sS', function() vscode.action('workbench.action.showAllSymbols') end)
+    vim.keymap.set('n', '<leader>c', function() vscode.action('workbench.action.showCommands') end)
+    vim.keymap.set('n', '<leader>e', function() vscode.action('workbench.view.explorer') end)
+
+    vim.keymap.set('n', '<leader>gd', function() vscode.action('editor.action.revealDefinition') end)
+    
+    vim.keymap.set('n', '<leader>i', function()
+        vscode.action('editor.action.showHover')
+    end)
+    vim.keymap.set('n', '<leader>gg', function() 
+        vscode.action('git-graph.view') 
+    end)
+
+    vim.keymap.set('n', '<leader>ts', function()
+        vscode.action('workbench.action.terminal.focus')
+    end)
+
+    vim.keymap.set('n', '<leader>tm', function()
+        vscode.action('workbench.action.terminal.focus')
+        vscode.action('workbench.action.toggleMaximizedPanel')
+    end)
 end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
-	require("plugins.bufferline"),
-	require("plugins.lualine"),
-	require("plugins.treesitter"),
-	require("plugins.lsp"),
-	require("plugins.telescope"),
-	require("plugins.autocompletion"),
-	require("plugins.autoformatting"),
-	require("plugins.gitsigns"),
-	require("plugins.misc"),
-	require("plugins.debug"),
-	require("plugins.markdown"),
-	require("plugins.zen"),
-	require("plugins.cloak"),
-	require("plugins.blankline"),
-	require("plugins.colortheme"),
-	require("plugins.oil"),
-})
